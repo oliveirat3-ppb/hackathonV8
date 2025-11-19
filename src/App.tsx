ffb5be5a36f80800fd09980facf526d82f07c5ae
oliveirat3-ppb <tomas.oliveira@paddypowerbetfair.com>
2025-11-19 10:46:22 +0000

    feat: frameworks

--- src/App.tsx
+++ src/App.tsx
@@ -9,56 +9,64 @@ function App() {
         </header>
 
         <main className="template-body">
-          {/* Package Manager */}
-          <section className="template-column">
-            <h2 className="column-title">Package Manager</h2>
-            <div className="card">
-              <div className="card-inner">
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-              </div>
-            </div>
-          </section>
-
           {/* FE Framework */}
           <section className="template-column">
             <h2 className="column-title">FE Framework</h2>
             <div className="card">
               <div className="card-inner">
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">React (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Typescript (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">React Router (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Redux (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
+                <div className="card-row">
+                  <span className="card-row-label">
+                    TanStack Query (vX.X.X)
+                  </span>
+                  <input type="checkbox" className="card-checkbox" />
+                </div>
+                <div className="card-row">
+                  <span className="card-row-label">GraphQL (vX.X.X)</span>
+                  <input type="checkbox" className="card-checkbox" />
+                </div>
+                <div className="card-row">
+                  <span className="card-row-label">
+                    React Bootstrap (vX.X.X)
+                  </span>
+                  <input type="checkbox" className="card-checkbox" />
+                </div>
+                <div className="card-row">
+                  <span className="card-row-label">Material UI (vX.X.X)</span>
+                  <input type="checkbox" className="card-checkbox" />
+                </div>
+                <div className="card-row">
+                  <span className="card-row-label">Ivory (vX.X.X)</span>
+                  <input type="checkbox" className="card-checkbox" />
+                </div>
+                <div className="card-row">
+                  <span className="card-row-label">Fabric (vX.X.X)</span>
+                  <input type="checkbox" className="card-checkbox" />
+                </div>
+              </div>
+            </div>
+          </section>
+
+          {/* Package Manager */}
+          <section className="template-column">
+            <h2 className="column-title">Package Manager</h2>
+            <div className="card">
+              <div className="card-inner">
                 <div className="card-row">
                   <span className="card-row-label">NPM (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
@@ -73,23 +81,11 @@ function App() {
             <div className="card">
               <div className="card-inner">
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Vite (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
-                  <input type="checkbox" className="card-checkbox" />
-                </div>
-                <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Rollup (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
               </div>
@@ -102,23 +98,27 @@ function App() {
             <div className="card">
               <div className="card-inner">
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">ESLint (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Jest (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Vitest (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Playwright (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
                 <div className="card-row">
-                  <span className="card-row-label">NPM (vX.X.X)</span>
+                  <span className="card-row-label">Husky (vX.X.X)</span>
+                  <input type="checkbox" className="card-checkbox" />
+                </div>
+                <div className="card-row">
+                  <span className="card-row-label">MSW (vX.X.X)</span>
                   <input type="checkbox" className="card-checkbox" />
                 </div>
               </div>
