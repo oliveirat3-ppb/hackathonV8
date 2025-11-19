e7eaf21cbc23496b133c55977fc49a40da7ab4a5
Marco Silva <marco.silva@paddypowerbetfair.com>
2025-11-19 11:03:25 +0000

    add PackageVersion component

---
+++ src/PackageVersion.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+export default function PackageVersion({ packageName }: { packageName: string }) {
+  const [version, setVersion] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchVersion(pkgName: string) {
+      const res = await fetch(`https://registry.npmjs.org/${pkgName}/latest`);
+
+      if (!res.ok) {
+        try{
+          const errorData = await res.json();
+          setError(errorData);
+          setVersion('');
+        } catch {
+          console.error(`We found an error when parsing information for ${pkgName}`)
+        }
+      } else {
+        const data = await res.json();
+        setVersion(data.version);
+        setError("");
+      }
+    }
+    fetchVersion(packageName);
+  }, [packageName]);
+
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        padding: "10px",
+        borderRadius: "5px",
+        marginTop: "10px",
+      }}
+    >
+      <strong>{packageName}</strong>{" "}
+      {error ? `Error: ${error}` : `latest version: ${version || "Loading..."}`}
+    </div>
+  );
+};
